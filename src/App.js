c414bec07132ec390fa5b2702323dba8c4d97f8c
basahithya <learningsahithya@gmail.com>
2022-08-29 23:07:18 +0530

    Services Component Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Intro from './components/Intro/Intro';
+import Services from './components/Services/Services';
 function App() {
   return (
     <div className="App">
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Services.css';
 
 const Services = () => {
     return (
         <div className='services'>
+            {/* Left Side */}
+            <div className="awesome">
+                <span>My Awesome</span>
+                <span>Services</span>
+                <button className='button i-button'>Hire Me</button>
+            </div>
             
+            {/* Right Side */}
+            <div className="cards">
+                Right Side
+            </div>
         </div>
     );
 };
